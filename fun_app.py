f4426132c0e147eeb73d10d0c4e3f36a28d35c5a
Dandanmian02 <chenzhanqidean2010@gmail.com>
2024-08-14 17:29:25 -0700

    add it two new files
    fun_app
    streamlit_Guessing_Game

---
+++ fun_app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title('A fun website')
+st.title('_streamlit_ is :green[cool] :sunglasses:')
+st.write('This is an app')
+st.title(':star2: What _the_ :blue[creator] say: :star2:')
+st.write('I _can_ :pink[create] a website similar to Streamlit, which has some practical :star2: addresses for learning Python programming and can help\n beginners quickly understand what programming is and how to learn it.\n This website will have some "dictionaries" of Python code,\n which will have the usage and purpose of the code, and will include the "syntax" of the code (in the future, upgrades will also become websites similar to Youdao Dictionary [Python version]) ' )
