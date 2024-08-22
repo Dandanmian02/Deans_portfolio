799bb10a0c65cad831a47a773028b3f990042da8
Dandanmian02 <chenzhanqidean2010@gmail.com>
2024-08-21 19:01:56 -0700

    add basic streamlit element

--- main.py
+++ main.py
@@ -2,31 +2,7 @@ import streamlit as st
 
 from src.introduction_1 import introduction
 from src.setup_2 import setup
-
-# Basic streamlit elements
-def basic_streamlit_elements():
-    st.header(':blue[3] . Basic Streamlit Elements:man-boy-boy:')
-    st.write('Let\'s explore some basic Streamlit elements:')
-    st.code('''
-    st.text('Fixed width text')
-    st.markdown('_Markdown_') # see #*
-    st.caption('Balloons. Hundreds of them...')
-    st.latex(r""" e^{i\pi} + 1 = 0 """)
-    st.write('Most objects') # df, err, func, keras!
-    st.write(['st', 'is <', 3]) # see *
-    st.title('My title')
-    st.header('My header')
-    st.subheader('My sub')
-    st.code('for i in range(8): foo()')
-    ''')
-    st.subheader(':blue[3] . :red[2] Input Elements:man-girl-boy:')
-    st.write("Streamlit provides various input elements:")
-    name = st.text_input('Please enter your name')
-    age = st.slider('Select your age', 10, 15)
-    st.write(f'Hello :blue[{name}] you are :red[{age}] year old.')
-    st.subheader('3.3 Display Image')
-    st.write('You can display images like this.')
-    st.image('https://pic1.zhimg.com/v2-a678cbd60508ae8907303154fb2340ac_r.jpg')
+from src.basic_streamlit_elements_3 import basic_stremlit_elements
 
 # Interactive app
 def interactive_app():
